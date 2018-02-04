27b61f8b6324468f48520eab211039ea83d9a67d
Peefy <xpf6677@163.com>
2018-02-04 10:01:17 +0800

    Update nuget to v0.0.2
    Update nuget to v0.0.2

---
+++ examples/ArkLight.Example.NetStandard/ObjectObservableRangeCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ArkLight.Collection;
+
+namespace ArkLight.Example
+{
+    public class ObjectObservableRangeCollection : ObservableRangeCollection<object>
+    {
+        public ObjectObservableRangeCollection()
+        {
+            this.Add(new object());
+            this.Add(default);
+            this.Add(default);
+        }
+    }
+}
